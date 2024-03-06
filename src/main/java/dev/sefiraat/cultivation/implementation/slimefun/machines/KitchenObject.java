5d3d46c59ee9f5bb6841049cc097ec181a8e4b87
JustAHuman-xD <65748158+JustAHuman-xD@users.noreply.github.com>
2024-03-06 11:16:25 -0600

    continue burning

--- src/main/java/dev/sefiraat/cultivation/implementation/slimefun/machines/KitchenObject.java
+++ src/main/java/dev/sefiraat/cultivation/implementation/slimefun/machines/KitchenObject.java
@@ -2,20 +2,14 @@ package dev.sefiraat.cultivation.implementation.slimefun.machines;
 
 import dev.sefiraat.sefilib.entity.display.DisplayGroup;
 import dev.sefiraat.sefilib.slimefun.blocks.DisplayGroupBlock;
-import io.github.thebusybiscuit.slimefun4.api.events.PlayerRightClickEvent;
 import io.github.thebusybiscuit.slimefun4.api.items.ItemGroup;
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
 import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
-import io.github.thebusybiscuit.slimefun4.core.handlers.BlockUseHandler;
 import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.block.Block;
-import org.bukkit.entity.Display;
-import org.bukkit.entity.ItemDisplay;
 import org.bukkit.inventory.ItemStack;
 
 import javax.annotation.Nonnull;
-import javax.annotation.ParametersAreNonnullByDefault;
 import java.util.Set;
 import java.util.function.Function;
 
@@ -47,56 +41,6 @@ public abstract class KitchenObject extends DisplayGroupBlock {
     @Override
     public void preRegister() {
         super.preRegister();
-        addItemHandler(
-            new BlockUseHandler() {
-                @Override
-                public void onRightClick(PlayerRightClickEvent e) {
-                    KitchenObject.this.onRightClick(e);
-                }
-            }
-        );
-    }
-
-    protected void onRightClick(@Nonnull PlayerRightClickEvent event) {
-        if (event.getPlayer().isSneaking()) {
-            ItemStack mainHand = event.getItem();
-            ItemStack offHand = event.getPlayer().getInventory().getItemInOffHand();
-            Block block = event.getClickedBlock().orElse(null);
-
-            if (block == null) {
-                return;
-            }
-
-            if (mainHand != null && !mainHand.getType().isAir()) {
-                trySetTop(block.getLocation(), mainHand);
-            }
-
-            if (offHand != null && !offHand.getType().isAir()) {
-                trySetBase(block.getLocation(), offHand);
-            }
-        }
-    }
-
-    public void trySetTop(@Nonnull Location location, @Nonnull ItemStack itemStack) {
-        trySetObject(location, itemStack, "counter_top");
-    }
-
-    public void trySetBase(@Nonnull Location location, @Nonnull ItemStack itemStack) {
-        trySetObject(location, itemStack, "counter_base");
-    }
-
-    @ParametersAreNonnullByDefault
-    private void trySetObject(Location location, ItemStack itemStack, String name) {
-        if (itemStack.getType().isBlock() && itemStack.getType().isOccluding()) {
-            DisplayGroup displayGroup = getDisplayGroup(location);
-            if (displayGroup == null) {
-                return;
-            }
-            Display display = displayGroup.getDisplays().get(name);
-            if (display instanceof ItemDisplay itemDisplay) {
-                itemDisplay.setItemStack(itemStack.clone());
-            }
-        }
     }
 
     public static boolean isBucket(@Nonnull ItemStack itemStack) {
--- src/main/java/dev/sefiraat/cultivation/implementation/slimefun/machines/RightClickKitchenMachine.java
+++ src/main/java/dev/sefiraat/cultivation/implementation/slimefun/machines/RightClickKitchenMachine.java
@@ -1,13 +1,10 @@
 package dev.sefiraat.cultivation.implementation.slimefun.machines;
 
 import dev.sefiraat.sefilib.entity.display.DisplayGroup;
-import io.github.thebusybiscuit.slimefun4.api.events.PlayerRightClickEvent;
 import io.github.thebusybiscuit.slimefun4.api.items.ItemGroup;
-import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItem;
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
 import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
 import org.bukkit.Location;
-import org.bukkit.block.Block;
 import org.bukkit.inventory.ItemStack;
 
 import javax.annotation.Nonnull;
@@ -34,28 +31,4 @@ public class RightClickKitchenMachine extends KitchenRecipeMachineSimple {
         return recipes;
     }
 
-    @Override
-    protected void onRightClick(@Nonnull PlayerRightClickEvent event) {
-        if (event.getPlayer().isSneaking()) {
-            super.onRightClick(event);
-        }
-        ItemStack itemStack = event.getItem();
-        SlimefunItem slimefunItem = SlimefunItem.getByItem(itemStack);
-        String id = itemStack.getType().name();
-        if (slimefunItem != null) {
-            id = slimefunItem.getId();
-        }
-
-        ItemStack result = testRecipe(id);
-        Block block = event.getClickedBlock().orElse(null);
-
-        if (block == null || result == null) {
-            return;
-        }
-
-        Location location = block.getLocation().clone().add(0.5, 1, 0.5);
-        location.getWorld().dropItem(location, result);
-        itemStack.setAmount(itemStack.getAmount() - 1);
-
-    }
 }
